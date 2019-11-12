0d4a380666eed1028699072bf7ba16061d986af5
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-11-12 17:34:33 +0100

    manage preview

--- src/main/resources/helm/helmrunner.py
+++ src/main/resources/helm/helmrunner.py
@@ -12,74 +12,94 @@ import json
 from overtherepy import OverthereHostSession
 import sys
 
+class HelmRunner:
 
-def get_helm_command(helmclient):
-    helm = '{0}/helm'.format(helmclient.home)
+    def __init__(self,helmclient):
+        self.helmclient = helmclient
 
-    if helmclient.kubeContext is not None:
-        helm = helm + ' --kube-context {0}'.format(helmclient.kubeContext)
-    if helmclient.kubeConfig is not None:
-        helm = helm + ' --kubeconfig {0}'.format(helmclient.kubeConfig)
-    if helmclient.helmHost is not None:
-        helm = helm + ' --host {0}'.format(helmclient.helmHost)
-    if helmclient.debug:
-        helm = helm + ' --debug'
-    return helm
+    def get_helm_command(self):
+        helm = '{0}/helm'.format(self.helmclient.home)
+
+        if self.helmclient.kubeContext is not None:
+            helm = helm + ' --kube-context {0}'.format(self.helmclient.kubeContext)
+        if self.helmclient.kubeConfig is not None:
+            helm = helm + ' --kubeconfig {0}'.format(self.helmclient.kubeConfig)
+        if self.helmclient.helmHost is not None:
+            helm = helm + ' --host {0}'.format(self.helmclient.helmHost)
+        if self.helmclient.debug:
+            helm = helm + ' --debug'
+        return helm
 
 
-def get_install_parameters(session,deployed):
-    values = {'chartName':deployed.chartName,
-            'namespace':deployed.container.namespaceName,
-            'name':deployed.name,
-            'chartVersion': deployed.chartVersion}
 
-    parameters = "{chartName} --namespace {namespace}  --name {name} --version {chartVersion}".format(**values)
-    for cf in deployed.configurationFiles:
-        uploaded_file = session.upload_file_to_work_dir(cf.getFile())
-        parameters = parameters +" -f "+uploaded_file.getPath()
+    def command_line(self,session,deployed):
+        raise Exception("Not Implemented")
 
-    return parameters
 
-def get_upgrade_parameters(session,deployed):
-    values = {'chartName':deployed.chartName,
-            'namespace':deployed.container.namespaceName,
-            'name':deployed.name,
-            'chartVersion': deployed.chartVersion}
+    def preview(self,deployed):
+        try:
+            session = OverthereHostSession(self.helmclient.host,stream_command_output=False)
+            command_line = self.command_line(session,deployed)
+            print command_line
+        finally:
+            session.close_conn()
 
-    parameters = "{name} {chartName} --namespace {namespace} --version {chartVersion}".format(**values)
-    for cf in deployed.configurationFiles:
-        uploaded_file = session.upload_file_to_work_dir(cf.getFile())
-        parameters = parameters +" -f "+uploaded_file.getPath()
+    def execute(self,deployed):
+        try:
+            session = OverthereHostSession(self.helmclient.host,stream_command_output=False)
+            command_line = self.command_line(session,deployed)
+            print command_line
+            uploaded_runner = session.upload_text_content_to_work_dir(command_line,'xldeploy_helm.sh',executable=True)
+            print uploaded_runner.path
+            response = session.execute(command_line,check_success=False)
+            print "\n ".join(response.stdout)
+            print "\n ".join(response.stderr)
+            rc = response.rc
+            if response.rc > 0:
+                sys.exit(rc)
+        finally:
+            session.close_conn()
 
-    return parameters
 
-def get_install_commandline(helmclient,session, deployed):
-    command_line = "{0} install {1}".format(get_helm_command(helmclient),get_install_parameters(session,deployed))
-    return command_line
 
-def get_upgrade_commandline(helmclient,session, deployed):
-    command_line = "{0} upgrade {1}".format(get_helm_command(helmclient),get_upgrade_parameters(session,deployed))
-    return command_line
+class HelmInstall(HelmRunner):
 
-def unknown_commandline(helmclient,session, deployed):
-    raise Exception("unknown_commandline !! {0}".format(helmcommand))
+    def command_line(self,session,deployed):
+        return "{0} install {1}".format(self.get_helm_command(),self.parameters(session,deployed))
 
-try:
-    session = OverthereHostSession(helmclient.host,stream_command_output=False)
-    #TODO manage streams
-    command_lines = {
-            'install':get_install_commandline,
-            'upgrade':get_upgrade_commandline}
-    command_line = command_lines.get(helmcommand, unknown_commandline)(helmclient,session,deployed)
-    print command_line
-    uploaded_runner = session.upload_text_content_to_work_dir(command_line,'xldeploy_helm.sh',executable=True)
-    print uploaded_runner.path
-    response = session.execute(command_line,check_success=False)
-    print "\n ".join(response.stdout)
-    rc = response.rc
-    if response.rc > 0:
-        sys.exit(rc)
-finally:
-    session.close_conn()
+    def parameters(self,session,deployed):
+        values = {'chartName':deployed.chartName,
+                'namespace':deployed.container.namespaceName,
+                'name':deployed.name,
+                'chartVersion': deployed.chartVersion}
+
+        parameters = "{chartName} --namespace {namespace}  --name {name} --version {chartVersion}".format(**values)
+        for cf in deployed.configurationFiles:
+            uploaded_file = session.upload_file_to_work_dir(cf.getFile())
+            parameters = parameters +" -f "+uploaded_file.getPath()
+
+        return parameters
+
+
+
+class HelmUpgrade(HelmRunner):
+
+    def command_line(self,session,deployed):
+        return "{0} upgrade {1}".format(self.get_helm_command(),self.parameters(session,deployed))
+
+    def parameters(self,session,deployed):
+        values = {'chartName':deployed.chartName,
+                'namespace':deployed.container.namespaceName,
+                'name':deployed.name,
+                'chartVersion': deployed.chartVersion}
+
+        parameters = "{name} {chartName} --namespace {namespace} --version {chartVersion}".format(**values)
+        for cf in deployed.configurationFiles:
+            uploaded_file = session.upload_file_to_work_dir(cf.getFile())
+            parameters = parameters +" -f "+uploaded_file.getPath()
+
+        return parameters
+
+
 
 
